8fe0ae5d42eecab6943d7ca9639a5d3a7211c08a
luiz.carrion <luiz.carrion@ip.tv>
2023-07-13 19:02:22 -0300

    redirect from profile to index

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,5 +1,7 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -19,6 +21,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+  const router = useRouter();
+  const { data: user } = useCurrentUser();
+
   return (
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
@@ -27,7 +32,7 @@ const Profiles = () => {
         </h1>
 
         <div className="flex items-center justify-center gap-8 mt-10">
-          <div onClick={() => {}}>
+          <div onClick={() => router.push("/")}>
             <div className="group flex-row w-44 mx-auto">
               <div
                 className="
@@ -56,7 +61,7 @@ const Profiles = () => {
                     group-hover:text-white
                 "
               >
-                Name
+                {user?.name}
               </div>
             </div>
           </div>
