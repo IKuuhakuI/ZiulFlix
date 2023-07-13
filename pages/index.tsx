d3da83241a5b31b1dda1d58bf4b6965f9498b344
luiz.carrion <luiz.carrion@ip.tv>
2023-07-13 18:12:10 -0300

    redirect logged out users

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-green-500">ZiulFlix</h1>
+      <h1 className="text-4xl text-green-500">ZiulFlix</h1>
+
+      <button onClick={() => signOut()} className="h-10 w-full bg-white">
+        Logout
+      </button>
     </>
   );
 }
