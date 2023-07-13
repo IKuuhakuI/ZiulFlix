8cd52cf6e7a11b957ac23f23d1adf4611bac741c
luiz.carrion <luiz.carrion@ip.tv>
2023-07-13 15:40:49 -0300

    toggle Login and Register page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,18 @@
 import Input from "@/components/Input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
@@ -14,15 +22,19 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign In" : "Register"}
+            </h2>
 
             <div className="flex flex-col gap-4">
-              <Input
-                label="Username"
-                onChange={(ev: any) => setName(ev.target.value)}
-                id="name"
-                value={name}
-              />
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(ev: any) => setName(ev.target.value)}
+                  id="name"
+                  value={name}
+                />
+              )}
 
               <Input
                 label="E-mail"
@@ -40,6 +52,30 @@ const Auth = () => {
                 value={password}
               />
             </div>
+
+            <button
+              className="
+                    bg-red-600 py-3 
+                    text-white 
+                    rounded-md 
+                    w-full
+                    mt-10
+                    hover:bg-red-700
+                    transition
+                "
+            >
+              {variant === "login" ? "Login" : "Sign Up"}
+            </button>
+
+            <p className="text-neutral-500 mt-12">
+              First time using ZiulFlix?
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                Create an account
+              </span>
+            </p>
           </div>
         </div>
       </div>
