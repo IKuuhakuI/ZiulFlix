8bcbfac363764d8db397132ee2136323b0bf685b
luiz.carrion <luiz.carrion@ip.tv>
2023-07-13 15:48:12 -0300

    fix class background color name

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -16,12 +16,12 @@ const Auth = () => {
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
-      <div className="bg-black w-full h-full lg: bg-opacity-50">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
           <img src="/images/logo.png" alt="Logo" className="h-12" />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:2/5 lg:max-w-md rounded-md w-full">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
               {variant === "login" ? "Sign In" : "Register"}
             </h2>
@@ -68,12 +68,14 @@ const Auth = () => {
             </button>
 
             <p className="text-neutral-500 mt-12">
-              First time using ZiulFlix?
+              {variant === "login"
+                ? "First time using ZiulFlix?"
+                : "Already have an account?"}
               <span
                 onClick={toggleVariant}
                 className="text-white ml-1 hover:underline cursor-pointer"
               >
-                Create an account
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
             </p>
           </div>
