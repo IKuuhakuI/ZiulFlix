2f8f15890a4cc98549d35eebbd359c3c9ffb454c
luiz.carrion <luiz.carrion@ip.tv>
2023-07-16 13:48:48 -0300

    show movie thumbnail on trending now

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 interface MovieCardProps {
@@ -5,7 +6,26 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
-  return <div></div>;
+  return (
+    <div className="group bg-zinc-900 col-span relative h-[12vw]">
+      <img
+        className="
+        cursor-pointer
+        object-cover
+        transition
+        durantion
+        shadow-xl
+        rounded-md
+        group-hover:opacity-90
+        sm:group-hover:opacity-0
+        w-full
+        h-[12vw]
+      "
+        src={data.thumbnailUrl}
+        alt="Thumbnail"
+      />
+    </div>
+  );
 };
 
 export default MovieCard;
